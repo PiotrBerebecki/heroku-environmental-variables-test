6c96b627d8b69e7d7481537ccde4224bd4d1ed8d
PiotrBerebecki <petebere@gmail.com>
2017-03-14 16:02:54 +0000

    Add secret key

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 var http = require('http');
 var router = require('./router/router');
 
+require('env2')('.env');
+console.log(process.env.MY_SECRET_AMAZON_KEY);
+
+
 var port = process.env.PORT || 3000;
 
 var server = http.createServer(router);
